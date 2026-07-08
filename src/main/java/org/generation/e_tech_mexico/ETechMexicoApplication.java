b15f1ade71e0c6f0830daca3967a3218a4f23f4c
JMV76489 <95047311+JMV76489@users.noreply.github.com>
2026-07-08 10:08:07 -0600

    feat: agregar archivos iniciales del proyecto

---
+++ src/main/java/org/generation/e_tech_mexico/ETechMexicoApplication.java
@@ -0,0 +1,13 @@
+package org.generation.e_tech_mexico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETechMexicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETechMexicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/e_tech_mexico/ETechMexicoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.e_tech_mexico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETechMexicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
