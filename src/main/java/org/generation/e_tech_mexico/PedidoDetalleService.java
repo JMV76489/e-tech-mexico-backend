0ccd26235d59c3286df813f5705dea4b0d12057c
Nathaly Alvarez <alvarezquinteronathaly@gmail.com>
2026-07-08 13:39:48 -0600

    Se crea archivo pedidodetalleservice y pedido detalle

---
+++ src/main/java/org/generation/e_tech_mexico/PedidoDetalleService.java
@@ -0,0 +1,12 @@
+package com.etech.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoDetalleService {
+
+    public String mensaje() {
+        return "Servicio de PedidoDetalle funcionando";
+    }
+
+}
