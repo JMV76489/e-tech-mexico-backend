465e74c8253dd7e63173dd787cae0679a8a136d3
Nathaly Alvarez <alvarezquinteronathaly@gmail.com>
2026-07-08 16:56:25 -0600

    Se crea archivo pedidodetalleservice

--- src/main/java/org/generation/e_tech_mexico/PedidoDetalleService.java
+++ src/main/java/org/generation/e_tech_mexico/PedidoDetalleService.java
@@ -1,12 +1,27 @@
 package com.etech.service;
 
+import com.etech.model.PedidoDetalle;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PedidoDetalleService {
 
-    public String mensaje() {
-        return "Servicio de PedidoDetalle funcionando";
+    public List<PedidoDetalle> obtenerTodos() {
+        return new ArrayList<>();
     }
 
+    public PedidoDetalle obtenerPorId(Long id) {
+        return null;
+    }
+
+    public PedidoDetalle guardar(PedidoDetalle pedidoDetalle) {
+        return pedidoDetalle;
+    }
+
+    public void eliminar(Long id) {
+
+    }
 }
