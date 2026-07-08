0f06ce1cf3e966307a7ab985691070c709472a65
JMV76489 <95047311+JMV76489@users.noreply.github.com>
2026-07-08 16:15:23 -0600

    refactor: cambiar fechas a LocalDateTime

--- src/main/java/org/generation/e_tech_mexico/modelo/Pedido.java
+++ src/main/java/org/generation/e_tech_mexico/modelo/Pedido.java
@@ -1,21 +1,23 @@
 package org.generation.e_tech_mexico.modelo;
 
+import java.time.LocalDateTime;
+
 //Todo realizar Jose
 public class Pedido {
 
     // Mapeo de campos de la tabla SQL 'pedido'
     private Long idPedido;      // `id_pedido` INT
     private Long idUsuario;     // `id_usuario` INT
-    private String fechaPedido; // `fecha_pedido` DATETIME (Simulado como String)
+    private LocalDateTime fechaPedido; // `fecha_pedido` DATETIME (Simulado como String)
     private Double total;       // `total` DECIMAL(10,2)
-    private String fechaEntrega;// `fecha_entrega` DATETIME (Simulado como String)
+    private LocalDateTime fechaEntrega;// `fecha_entrega` DATETIME (Simulado como String)
 
     // Constructor vacio
     public Pedido(){
     }// Constructor Pedido vacio
 
     // Constructor con todos los parámetros (Ideal para simular datos en el servicio)
-    public Pedido(Long idPedido, Long idUsuario, String fechaPedido, Double total, String fechaEntrega){
+    public Pedido(Long idPedido, Long idUsuario, LocalDateTime fechaPedido, Double total, LocalDateTime fechaEntrega){
         this.idPedido = idPedido;
         this.idUsuario = idUsuario;
         this.fechaPedido = fechaPedido;
@@ -41,11 +43,11 @@ public class Pedido {
         this.idUsuario = idUsuario;
     }
 
-    public String getFechaPedido() {
+    public LocalDateTime getFechaPedido() {
         return fechaPedido;
     }
 
-    public void setFechaPedido(String fechaPedido) {
+    public void setFechaPedido(LocalDateTime fechaPedido) {
         this.fechaPedido = fechaPedido;
     }
 
@@ -57,11 +59,11 @@ public class Pedido {
         this.total = total;
     }
 
-    public String getFechaEntrega() {
+    public LocalDateTime getFechaEntrega() {
         return fechaEntrega;
     }
 
-    public void setFechaEntrega(String fechaEntrega) {
+    public void setFechaEntrega(LocalDateTime fechaEntrega) {
         this.fechaEntrega = fechaEntrega;
     }
 
