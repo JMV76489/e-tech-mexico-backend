ded5c9c9e1d62785d50348d555799900810763e4
Mrbrightside <alesg9764@gmail.com>
2026-07-09 10:32:33 -0600

    creacion de la entidad Usuario y la logica de UsuarioService

--- src/main/java/org/generation/e_tech_mexico/modelo/Usuario.java
+++ src/main/java/org/generation/e_tech_mexico/modelo/Usuario.java
@@ -1,5 +1,54 @@
 package org.generation.e_tech_mexico.modelo;
 
-//Todo Alejandro
 public class Usuario {
+
+    private Long id;
+    private String nombre;
+    private String email;
+    private String password;
+
+    // Constructor vacío (obligatorio para Spring Boot)
+    public Usuario() {
+    }
+
+    // Constructor con parámetros
+    public Usuario(Long id, String nombre, String email, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
