5eaec73c6df8ba52c786824a01ccb994dce9af48
JMV76489 <95047311+JMV76489@users.noreply.github.com>
2026-07-15 15:51:51 -0600

    feat: agregar cotnrolador de Login.

---
+++ src/main/java/org/generation/e_tech_mexico/controlador/LoginController.java
@@ -0,0 +1,33 @@
+package org.generation.e_tech_mexico.controlador;
+
+import jakarta.servlet.ServletException;
+import org.generation.e_tech_mexico.dto.LoginRequest;
+import org.generation.e_tech_mexico.dto.TokenAcceso;
+import org.generation.e_tech_mexico.servicio.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/login/")
+public class LoginController {
+
+    private final UsuarioService usuarioService;
+
+    @Autowired
+    public LoginController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public TokenAcceso validarUsuario(@RequestBody LoginRequest request) throws ServletException {
+        if (usuarioService.validarUsuario(request)) {
+            TokenAcceso tokenAcceso = new TokenAcceso(usuarioService.generarToken(request.getEmail()));
+            return tokenAcceso;
+        }
+
+        throw new ServletException("Login Error: el correo o la contraseña son inválidos");
+    }
+}
