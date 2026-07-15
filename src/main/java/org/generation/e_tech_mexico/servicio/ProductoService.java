d33b04e61f92bb22cb081a2666eceec0ce31ab54
JMV76489 <95047311+JMV76489@users.noreply.github.com>
2026-07-15 10:32:38 -0600

    feat: agregar funciones para ProductoService

--- src/main/java/org/generation/e_tech_mexico/servicio/ProductoService.java
+++ src/main/java/org/generation/e_tech_mexico/servicio/ProductoService.java
@@ -1,6 +1,8 @@
 package org.generation.e_tech_mexico.servicio;
 
 import org.generation.e_tech_mexico.modelo.Producto;
+import org.generation.e_tech_mexico.repositorio.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -10,47 +12,49 @@ import java.util.List;
 @Service
 public class ProductoService {
 
-    private final ArrayList<Producto> lista = new ArrayList<>();
+    private final ProductoRepository productoRepository;
+
+    @Autowired
+    public ProductoService(ProductoRepository productoRepository) {
+        this.productoRepository = productoRepository;
+    }
 
     public Producto createProducto(Producto producto) {
-        return lista.add(producto) ? producto : null;
+        return productoRepository.save(producto);
     }
 
     public List<Producto> getProductos() {
-        return lista;
+        return productoRepository.findAll();
     }
 
-    public Producto getProducto(Long idProducto) {
-        return lista
-                .stream()
-                .filter(producto -> producto.getIdProducto().equals(idProducto))
-                .findFirst()
-                .orElse(null);
+    public Producto getProducto(Long id) {
+        return productoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("El producto con el id [" + id + "] no existe"));
     }
 
     public Producto updateProducto(Long id, String nombre, String descripcion, Double precio, String categoria, String urlImagen, Integer stock, LocalDateTime fechaCreacion) {
-        for (Producto producto : lista) {
-            if (producto.getIdProducto().equals(id)) {
-                if (nombre != null) producto.setNombre(nombre);
-                if (descripcion != null) producto.setDescripcion(descripcion);
-                if (precio != null) producto.setPrecio(precio);
-                if (categoria != null) producto.setCategoria(categoria);
-                if (urlImagen != null) producto.setUrlImagen(urlImagen);
-                if (stock != null) producto.setStock(stock);
-                if (fechaCreacion != null) producto.setFechaCreacion(fechaCreacion);
-                return producto;
-            }
+        if (productoRepository.existsById(id)) {
+            Producto producto = productoRepository.findById(id).get();
+
+            if (nombre != null) producto.setNombre(nombre);
+            if (descripcion != null) producto.setDescripcion(descripcion);
+            if (precio != null) producto.setPrecio(precio);
+            if (categoria != null) producto.setCategoria(categoria);
+            if (urlImagen != null) producto.setUrlImagen(urlImagen);
+            if (stock != null) producto.setStock(stock);
+            if (fechaCreacion != null) producto.setFechaCreacion(fechaCreacion);
+
+            return productoRepository.save(producto);
         }
         return null;
     }
 
-    public Producto deleteProducto(Long idProducto) {
-        for (Producto producto : lista) {
-            if (producto.getIdProducto().equals(idProducto)) {
-                lista.remove(producto);
-                return producto;
-            }
+    public Producto deleteProducto(Long id) {
+        if (productoRepository.existsById(id)) {
+            Producto deletedProducto = productoRepository.findById(id).get();
+            productoRepository.deleteById(id);
+            return deletedProducto;
         }
+
         return null;
     }
 }
